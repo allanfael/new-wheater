bf13712c78e7bdd477479f888d2a7f88b28a116c
Allan Rafael <allanrafael@MacBook-Air-de-Allan.local>
2022-06-22 12:40:37 -0300

    fix: fix padding top

--- src/screens/Home/components/Header/styles.ts
+++ src/screens/Home/components/Header/styles.ts
@@ -4,5 +4,5 @@ import { marginTop } from '@utils/size';
 export const Container = styled.View`
   align-items: flex-end;
   width: 100%;
-  margin-top: ${marginTop(10)}px;
+  margin-top: ${marginTop(40)}px;
 `;
